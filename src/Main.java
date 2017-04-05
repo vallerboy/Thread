f11e0478f4fc327a2a3343131c0769866949d5b6
Oskar Pol <oskarbank@gmail.com>
2017-04-05 18:28:31 +0200

    ExecutorService

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * Created by OskarPraca on 2017-04-04.
  */
 public class Main {
     public static void main(String[] args) {
-
-
         Runnable r = new Runnable() {
             @Override
             public void run() {
@@ -28,16 +29,44 @@ public class Main {
       //  new Thread(r).start();
       //  new Thread(r1).start();
       //  System.out.println("Start time!");
+        new Main();
+    }
 
-        Operation operation = new Operation();
-        for(int i = 0; i < 100; i++) {
-             new Thread1(operation).start();
-        }
+    private ExecutorService executorService;
+
+    public Main() {
+          executorService = Executors.newSingleThreadExecutor();
+
+        Runnable runnable = new Runnable() {
+              @Override
+              public void run() {
+                  for(int i = 0; i <= 1000; i++) {
+                      System.out.println("Jestem zadaniem 1");
+                  }
+              }
+          };
+
+        Runnable runnable1 = new Runnable() {
+            @Override
+            public void run() {
+                for(int i = 0; i <= 1000; i++) {
+                    System.out.println("Jestem zadaniem 2");
+                }
+            }
+        };
+
+        // Stare podejście
+        //new Thread(runnable).start();
+        //new Thread(runnable1).start();
+
+        //Nowe podejście
+        executorService.execute(runnable);
+        executorService.execute(runnable1);
     }
 
     private static class Operation {
         private int oskar = 0;
-         public int operation() {
+         synchronized public int operation() {
             oskar++;
             oskar--;
             return oskar;
